83fa474fc98c5b33d28e04c4f4f7c8ef0597041d
Jhonatan101136 <jhonatandossantos.w@gmail.com>
2022-08-24 21:57:55 -0300

    Atividade_02

---
+++ atividade_02/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          GUSTAVO LIMA
+      </header> 
+      <header className='App-data'>
+     <p> Data: 2 de agosto de 2019 (Sexta-Feira)</p>
+      </header>
+      <header className='App-horario'>
+     <p> Horário: 18h30 - 00h00 </p>
+      </header>
+      <header className='App-local'>
+     <p> Local: Parque do Povo </p>
+      </header>
+      <header className='App-cidade'>
+     <p> Cidade: Campina Grande/PR</p>
+      </header>
+      <header className='genero'>
+     <p> Gênero: Arte em forma de SERTANEJO</p>
+      </header>
+    </div>
+    
+  );
+
+}
+
+export default App;
